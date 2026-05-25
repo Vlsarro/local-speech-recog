50020e0223cbf4f32e0230a288cbbbc18d5fc836
Vladislav <Vlsarro@users.noreply.github.com>
2026-05-25 09:42:17 +0300

    implement asr call

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
+use std::fs::File;
+use std::io::Write;
 use std::{collections::HashSet, fs, path::Path, path::PathBuf};
 
-use anyhow::{Ok, Result};
+use anyhow::{Context, Ok, Result};
 use clap::Parser;
 use clap_verbosity_flag::{InfoLevel, Verbosity};
+
+use reqwest::Url;
+use reqwest::blocking::{Client, multipart};
 
 /// Whisper ASR Webservice client
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     /// Whisper ASR Webservice host
-    #[arg(long, default_value = "0.0.0.0:9005")]
+    #[arg(long, default_value = "http://0.0.0.0:9005")]
     host: String,
 
     /// Directory with audio or video files for transcription
@@ -78,6 +83,45 @@ fn list_media_files(dirpath: impl AsRef<Path>) -> Result<Vec<PathBuf>> {
     Ok(res)
 }
 
+const ASR_ENDPOINT: &str = "/asr";
+
+fn transcribe_file(http_client: &Client, path: &Path, args: &Args) -> Result<String> {
+    let url = Url::parse_with_params(
+        &args.host,
+        &[
+            ("encode", "true"),
+            ("task", "transcribe"),
+            ("language", &args.lang),
+            ("initial_prompt", ""),
+            ("word_timestamps", "false"),
+            ("output", &args.output),
+        ],
+    )?
+    .join(ASR_ENDPOINT)?;
+
+    let input_data = fs::read(path)?;
+
+    let form = multipart::Form::new().part(
+        "audio_file",
+        multipart::Part::bytes(input_data).file_name("afile"),
+    );
+    let response = http_client
+        .post(url)
+        .multipart(form)
+        .send()?
+        .error_for_status()?;
+    let result_text = response.text()?;
+    Ok(result_text)
+}
+
+fn save_file(path: &Path, data: &str) -> Result<String> {
+    let txt_filepath = path.with_extension(TXT_FORMAT);
+    let txt_filepath_str = txt_filepath.to_string_lossy().into_owned();
+    let mut file = File::create(txt_filepath)?;
+    file.write_all(data.as_bytes())?;
+    Ok(txt_filepath_str)
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
     env_logger::Builder::new()
@@ -87,6 +131,23 @@ fn main() -> Result<()> {
     let media_files = list_media_files(&args.dirpath)?;
     let media_files_len = media_files.len();
     log::info!("Number of media files to process: {}", media_files_len);
+
+    let http_client = Client::new();
+    for (idx, filepath) in media_files.iter().enumerate() {
+        log::info!(
+            "[{}/{}] Processing {:?} ...",
+            idx + 1,
+            media_files_len,
+            filepath
+        );
+
+        let transcription_data = transcribe_file(&http_client, filepath, &args)
+            .with_context(|| format!("Failed to process {:?}", filepath))?;
+        let result_txt_filename = save_file(filepath, &transcription_data)
+            .with_context(|| format!("Failed to save data from processed {:?}", filepath))?;
+
+        log::info!("File transacription was saved to {}", result_txt_filename);
+    }
 
     Ok(())
 }
